713ac7e030364dd59a56992d962146e5ee93ad8c
Mike Redd <323583+MikereDD@users.noreply.github.com>
2026-06-17 17:05:59 -0500

    style(tool-menu): rename Media Encoder GUI to Media Encoder (GUI)
    Align the menu label with the (GUI) naming used by the other GUI
    entries (DVD Encoder (GUI), Blu-ray Encoder (GUI), CD FLAC Ripper (GUI)).

--- Windows/Powershell/scripts/menu/tool-menu.ps1
+++ Windows/Powershell/scripts/menu/tool-menu.ps1
@@ -94,7 +94,7 @@ $PersonalTools = @(
     [PSCustomObject]@{ Name="Blu-ray Encoder";       File="BRencoder.ps1" }
     [PSCustomObject]@{ Name="Blu-ray Encoder (GUI)"; File="BRencoder-gui.ps1" }
     [PSCustomObject]@{ Name="MKV Sample";            File="mkv-sample.ps1" }
-	[PSCustomObject]@{ Name="Media Encoder GUI";     File="media-encoder-gui.ps1" }
+	[PSCustomObject]@{ Name="Media Encoder (GUI)";     File="media-encoder-gui.ps1" }
     [PSCustomObject]@{ Name="WebRipper";             File="web-ripper.ps1" }
 )
 
